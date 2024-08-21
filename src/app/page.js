97052d5810735bb0a1e270b33b979d8938f414eb
Joseph Williams <jw69455@gmail.com>
2024-08-21 16:16:31 -0500

    added button images

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function Home() {
   const router = useRouter();
@@ -16,20 +17,20 @@ export default function Home() {
           </Avatar>
         </div>
         <h1 className="text-xl text-white">DIGITAL PARADISE MEDIA</h1>
-        <div className="flex gap-2 py-2 px-2">
+        <div className="flex gap-2 py-2 px-2 mt-8">
           <div>
             <Button onClick={() => router.push("/Services")}>
-              Services
+              <Image src="/services_button.jpeg" alt="Services" width={200} height={200} />
             </Button>
           </div>
           <div>
             <Button onClick={() => router.push("/About")}>
-              About
+            <Image src="/about_button.jpeg" alt="Services" width={200} height={200} />
             </Button>
           </div>
           <div>
             <Button onClick={() => router.push("/Tickets")}>
-              Tickets
+            <Image src="/ticketing_button.jpeg" alt="Services" width={200} height={200} />
             </Button>
           </div>
         </div>
