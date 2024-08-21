754c1f71f3a46bb30166d45e9620165cf6c1751a
Joseph Williams <jw69455@gmail.com>
2024-08-20 21:12:06 -0500

    Initial Commit

---
+++ src/app/About/page.js
@@ -0,0 +1,16 @@
+'use client';
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
+export default function About() {
+    const router = useRouter();
+
+    return (
+        <div className="flex h-screen w-full flex-col items-center px-4 py-6 relative">
+            <h1 className="text-xl text-white">ABOUT IN DEVELOPMENT</h1> 
+            <Button onClick={() => router.push("/")}>
+                Back
+            </Button>
+        </div>
+    );
+};
