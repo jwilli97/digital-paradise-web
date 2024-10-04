bcfd8dfa2d1b6a4ce905746856db2713c50dc3fb
Joseph Williams <jw69455@gmail.com>
2024-10-04 13:28:39 -0500

    cuervo updates

--- src/app/TicketConfirmation/page.js
+++ src/app/TicketConfirmation/page.js
@@ -2,6 +2,6 @@
 
 export default function TicketConfirmation() {
     return (
-        <div>Thank you for purchasing tickets! You'll receive an email confirmation that will contain your tickets shortly!</div>
+        <div>Thank you for purchasing tickets! You will receive an email confirmation that will contain your tickets shortly!</div>
     );
 };
